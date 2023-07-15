bc2dc4318507bf5163b7e263baeaec4af790ec08
Akiza <afifzakwanzaili@gmail.com>
2023-07-15 11:29:26 +0800

    adding home page and create page

---
+++ lib/view/home.dart
@@ -0,0 +1,123 @@
+//main function to combine all the other function.
+
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:ticketing_system/model/hour.dart';
+
+import 'package:flutter/services.dart' show rootBundle;
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ticketing System',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _regNoController = TextEditingController();
+  String _output = '';
+
+  Future<String> _loadJSONFromAsset(String filePath) async {
+    return await rootBundle.loadString(filePath);
+  }
+
+  void _processData() async {
+    String regNo = _regNoController.text.trim();
+
+    // Read the contents of the JSON file
+    String fileContents = await rootBundle.loadString('assets/json/file.json');
+    List<dynamic> data = jsonDecode(fileContents);
+
+    bool isCarRegistered = false;
+    String output = '';
+
+    for (var item in data) {
+      String carRegNo = item['reg_no'];
+      List<dynamic> details = item['details'];
+
+      if (carRegNo == regNo) {
+        isCarRegistered = true;
+        output += 'Registration Number: $carRegNo\n';
+
+        for (var detail in details) {
+          String checkIn = detail['checkin'];
+          String checkOut = detail['checkout'];
+
+          Hourly hour = Hourly(checkin: checkIn, checkout: checkOut);
+          WeekCategory weekCategory = WeekCategory();
+          output += 'In: $checkIn\n';
+          output += 'Out: $checkOut\n';
+          Payment payment = Payment(hour, weekCategory);
+
+          for (var detail in details) {
+            String checkIn = detail['checkin'];
+            String checkOut = detail['checkout'];
+            DateTime date = DateTime.parse(detail['checkout']);
+
+            double paymentAmount = payment.calculatePayment(date);
+            output +=
+                'Payment Amount: \$ ${paymentAmount.toStringAsFixed(2)}\n';
+
+            Validate validator = Validate(PayDetail(payment));
+
+            bool isValid = validator.validateTicket(date);
+            output += 'Ticket Valid: ${isValid ? 'Yes' : 'No'}\n';
+          }
+
+          break; // Exit the loop after finding a match
+        }
+      }
+    }
+
+    if (!isCarRegistered) {
+      output = 'The registration number $regNo is not found in the data.';
+    }
+    setState(() {
+      _output = output;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ticketing System'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _processData,
+                child: Text('Parking'),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _processData,
+                child: Text('Check Out'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
